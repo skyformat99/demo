aaabbc581e21bad1fbd1ab9069484f6dfa3c9268
Taylor.Commit <solarberiden@163.com>
2015-04-16 16:35:41 +0800

    add more fake seed users

--- server/seeds.js
+++ server/seeds.js
@@ -1,12 +1,16 @@
 Meteor.startup(function () {
     Posts.remove({});
   if (Posts.find({}).count() === 0) {
-      Posts.insert({
-          title: Fake.word(),
-          body: Fake.sentence(),
-          published: Fake.fromArray([true, false]),
-          authorId: this.userId
-      });
+
+      for(i = 0; i<5; i++){
+          Posts.insert({
+              title: Fake.word(),
+              body: Fake.sentence(),
+              published: Fake.fromArray([true, false]),
+              authorId: this.userId
+          });
+      }
+      
       //Posts.insert({
       //    title: Fake.word(),
       //    body: Fake.sentence(),
